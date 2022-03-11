2f1c2ebf89c70e8eb06878927709f8591e25d200
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-03-11 11:50:47 -0500

    Final v0.2

--- src/Components/Add Movie Form/AddMovieForm.js
+++ src/Components/Add Movie Form/AddMovieForm.js
@@ -5,26 +5,24 @@ import './AddMovieForm.css'
 
 function AddMovieForm (){
     return (
-    <div className="shity_movie">
-    <h1>Add your Shitty Movie</h1>
-    <form action="submit" method="post">
- <ul>
-  <li>
-    <label> Movie Name:</label>
-    <input type="text" className="movie_Name"/>
-  </li>
-  <li>
-    <label>Image Link:</label>
-    <input type="text" className="image_link" />
-  </li>
-  <li>
-    <label >Description:</label>
-    <textarea className="description" ></textarea>
-  </li>
-  <input type="submit" value="Submit"></input>
- </ul>
-</form>
-</div>
+    <form className="form" action="./Home" method="POST">
+      <div className="form_title">Shit Post</div>
+      <p> Add your worst movie and leave your stain on History!</p>
+      <div className="form_item">
+        <label For="moviename" className="form_label">Movie Name</label>
+        <input type="text" clasName="form_input" name="moviename" placeholder='Enter your Movie(s) Name'></input>
+      </div>
+      <div className="form_item">
+        <label For="movieimageurl" className="form_label">Movie Image Url</label>
+        <input type="url" clasName="form_input" name="movieimageurl" placeholder='Enter your Movie(s) Image URL'></input>
+      </div>
+      <div className="form_item">
+        <label For="moviedescription" className="form_label">Movie Description</label>
+       <textarea maxLength="500" className="form_input" name="moviedescription" id="moviedescription" placeholder="Enter your Movie Description. (Max. 500 Characters)"></textarea>
+      </div>
+ 
+ 
+    </form>
  )
 }
 
--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -2,31 +2,23 @@ import React from 'react'
 
 
 function ContactUs () {
- return(  
-     <div className='contact'>
-    <h1>Contact Us</h1>
-    <form action="submit" method="post">
- <ul>
-  <li>
-    <label> Name </label>
-    <input type="text" className="movie_Name"/>
-  </li>
-  <li>
-      <label>Email</label>
-      <input type="text" className="email"></input>
-  </li>
-  <li>
-    <label>Type of Issue</label>
-    <input type="text" className="image_link" />
-  </li>
-  <li>
-    <label >Issue Explanned</label>
-    <textarea className="description" ></textarea>
-  </li>
-  <input type="submit" value="Submit"></input>
- </ul>
-</form>
-</div>
+ return(    
+<form className="form" action="./Home" method="POST">
+      <div className="form_title">Contant Us</div>
+      <p>You should recieve a response in 1-3 buisness days</p>
+      <div className="form_item">
+        <label For="yournamr" className="form_label">Your Name</label>
+        <input type="text" clasName="form_input" name="yourname" placeholder='Enter your Movie(s) Name'></input>
+      </div>
+      <div className="form_item">
+        <label For="typeofissue" className="form_label">Type of Issue</label>
+        <input type="text" clasName="form_input" name="typeofissue" placeholder='Enter your Movie(s) Image URL'></input>
+      </div>
+      <div className="form_item">
+        <label For="issuedescription" className="form_label">Issue Description</label>
+       <textarea  maxLength="500" className="form_input" name="issuedescription" id="issuedescription" placeholder="Enter a detailed message about why you used to contact support. (Max. 500 Characters)"></textarea>
+      </div>
+    </form>
  )
 }
 
